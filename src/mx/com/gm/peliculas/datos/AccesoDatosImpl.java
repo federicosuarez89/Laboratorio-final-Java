f3b77c23799b6aa441e8e019152299dcf74a693a
Federico Leandro Suarez <federico.uni.89@gmail.com>
2023-01-24 10:45:16 -0300

    Creamos la clase que implementa la interfaz AccesoDatos y definimos el metodo listar para agregar las peliculas

--- src/mx/com/gm/peliculas/datos/AccesoDatosImpl.java
+++ src/mx/com/gm/peliculas/datos/AccesoDatosImpl.java
@@ -1,4 +1,66 @@
 package mx.com.gm.peliculas.datos;
 
-public class AccesoDatosImpl {
+import mx.com.gm.peliculas.domain.Pelicula;
+import mx.com.gm.peliculas.excepciones.*;
+import java.io.*;
+import java.util.*;
+
+public class AccesoDatosImpl implements AccesoDatos{
+
+
+    @Override
+    public boolean existe(String nombreRecurso) throws AccesoDatosEx {
+        File archivo = new File(nombreRecurso);
+        return archivo.exists();
+    }
+
+    @Override
+    public List<Pelicula> listar(String nombreRecurso) throws LecturaDatosEx {
+        File archivo = new File(nombreRecurso);
+        //Creamos la lista de peliculas
+        List<Pelicula> peliculas = new ArrayList<>();
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            //Leemos el texto ingresado
+            linea = entrada.readLine();
+            //Comprobamos que linea no este vacia para crear un objeto
+            while (linea !=null){
+                Pelicula pelicula = new Pelicula(linea);
+                //Agregamos un ojeto de tipo pelicula a la lista de peliculas
+                peliculas.add(pelicula);
+                //Leemos para ver si hay mas peliculas para agregar a la lista
+                linea = entrada.readLine();
+            }
+            //Cerramos el flujo
+            entrada.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas:"+e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas:"+e.getMessage());
+        }
+        return peliculas;
+    }
+
+    @Override
+    public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
+
+    }
+
+    @Override
+    public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
+        return null;
+    }
+
+    @Override
+    public void crear(String nombreRecurso) throws AccesoDatosEx {
+
+    }
+
+    @Override
+    public void borrar(String nombreRecurso) throws AccesoDatosEx {
+
+    }
 }
